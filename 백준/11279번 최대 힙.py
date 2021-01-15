b1e0ba169aa4f913213e8b0555c912b63c8c8e70
Seonggu Lee <gf265@naver.com>
2021-01-15 17:34:59 +0900

    update

--- 백준/11279번 최대 힙.py
+++ 백준/11279번 최대 힙.py
@@ -8,9 +8,9 @@ for _ in range(n):
     x = int(sys.stdin.readline().rstrip())
 
     if x != 0:
-        heapq.heappush(hq, -x)
+        heapq.heappush(hq, x)
     else:
         if hq:
-            print(-heapq.heappop(hq))
+            print(heapq.heappop(hq))
         else:
             print(0)
