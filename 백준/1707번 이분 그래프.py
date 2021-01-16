99aba9a3f4d1317bb7a4b47b07088e30c7db9922
Seonggu Lee <gf265@naver.com>
2021-01-16 16:34:41 +0900

    update

--- 백준/1707번 이분 그래프.py
+++ 백준/1707번 이분 그래프.py
@@ -10,7 +10,6 @@ for _ in range(t):
     adj = [[] for _ in range(v)]
     for _ in range(e):
         a, b = map(int, sys.stdin.readline().rstrip().split())
-
         adj[a-1].append(b-1)
         adj[b-1].append(a-1)
 
@@ -18,6 +17,7 @@ for _ in range(t):
 
     color = dict()
 
+    # bfs를 하면서 다른 같은 색깔이 겹치게 되면 False를 리턴한다. 이분 그래프인 경우 True 리턴
     def bfs(start):
         q = deque()
         q.append(start)
@@ -33,7 +33,7 @@ for _ in range(t):
                 elif color[there] == c:
                     return False
         return True
-
+    # 모든 정점을 방문하도록
     for i in range(v):
         if i not in color:
             if not bfs(i):
